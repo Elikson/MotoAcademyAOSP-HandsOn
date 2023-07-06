f238d399e3e6da5fb62acff679bb70b64ad94d7a
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2023-07-05 21:41:40 -0400

    config: firebase auth

--- tucano-app/app/src/main/java/com/rodrigo/tucano/LoginActivity.java
+++ tucano-app/app/src/main/java/com/rodrigo/tucano/LoginActivity.java
@@ -1,14 +1,51 @@
 package com.rodrigo.tucano;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.rodrigo.tucano.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.btnLogin.setOnClickListener(v -> {
+
+            mAuth.signInWithEmailAndPassword(binding.edtTextEmail.getText().toString(), binding.edtTextSenha.getText().toString())
+                    .addOnCompleteListener(
+                    this, task -> {
+                        if (task.isSuccessful()) {
+                            Log.e("Teste", "Sucesso");
+                            startActivity(new Intent(this, MainActivity.class));
+                            Toast.makeText(this, "login com sucesso", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Log.e("Teste", "Falha");
+                            Log.e("Teste", task.getResult().toString());
+                            Toast.makeText(this, "Falha no login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
+        });
+
     }
 }
